e3bd96c43f252b8b6c44b4eeb3a2006a5210dc32
luke0b1000 <35980314+luke0b1000@users.noreply.github.com>
2018-11-02 22:14:19 -0400

    minor refractor
    changing skip

--- problems/dafbcf_kangeroo/index.test.js
+++ problems/dafbcf_kangeroo/index.test.js
@@ -1,8 +1,8 @@
 const kangeroo = require('./index');
 
-test('luke test YES 1', () => {
+test.skip('luke test YES 1', () => {
     expect(kangeroo(0, 3, 4, 2)).toEqual('YES');
 });
-test('luke test NO 2', () => {
+test.skip('luke test NO 2', () => {
     expect(kangeroo(0, 2, 5, 3)).toEqual('NO');
 });
