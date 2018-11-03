44676c66611c56429ee9a6dbcd091f85e5d2d0da
luke0b1000 <35980314+luke0b1000@users.noreply.github.com>
2018-11-03 18:22:37 -0400

    savingbug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 /*
 https://www.hackerrank.com/challenges/sock-merchant/problem
 */
+function sockMerchant(sockArray) {
+    const sockTable = {};
+    // create a table to keep track of all types of sock
+    for (let i = 0; i < sockArray.length; i++) {
+        if (!sockTable[sockArray[i]]) sockTable[sockArray[i]] = 0;
+        sockTable[sockArray[i]]++;
+    }
+
+    let sellableSocks = 0;
+    // Math.floor each key, getting the total
+    const keys = Object.keys(sockTable); // sockTable[keys] = ['alpha', 'beta']
+    for (let i = 0; i < keys.length; i++) {
+        sellableSocks += Math.floor(sockTable[keys[i]] / 2);
+    }
+    return sellableSocks;
+}
+module.exports = sockMerchant;
---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+const sockMerchant = require(',/index');
+
+test('luke test 1', () => {
+    expect(sockMerchant([10, 20, 20, 10, 10, 30, 50, 10, 20])).toEqual(3);
+});
+test('luke test 1', () => {
+    expect(sockMerchant([1, 2, 1, 2, 1, 3, 2])).toEqual(2);
+});
