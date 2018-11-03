0fd5848141acb0d30c418e25979b9ac508e3b5bc
luke0b1000 <35980314+luke0b1000@users.noreply.github.com>
2018-11-02 21:05:16 -0400

    Completed Apple and Oranges
    Success! Just solved Apple and Orange on @HackerRank. Can you complete the challenge? http://hr.gs/1dqe #programming

---
+++ problems/1dqe_applesandoranges/index.js
@@ -0,0 +1,19 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    //calculate distance for apple + a
+    const appleFinal = apples.map(element => element + a);
+    // calculate distance for orange + b
+    const orangeFinal = oranges.map(element => element + b);
+    // loop through final apple and count how many are between s and t
+    let appleCount = appleFinal.reduce((accumulator, element) => {
+        if (element <= t && element >= s) accumulator += 1;
+        return accumulator;
+    }, 0);
+    // loop through final orange and count how many are between s and t
+    let orangeCount = orangeFinal.reduce((accumulator, element) => {
+        if (element <= t && element >= s) accumulator += 1;
+        return accumulator;
+    }, 0);
+    return [appleCount, orangeCount];
+}
+
+module.exports = countApplesAndOranges;
---
+++ problems/1dqe_applesandoranges/index.test.js
@@ -0,0 +1,8 @@
+const countApplesAndOranges = require('./index');
+
+test('luke test 1', () => {
+    expect(countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])).toEqual([
+        1,
+        1,
+    ]);
+});
