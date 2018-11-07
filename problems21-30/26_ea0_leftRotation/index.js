313b9782c4255b57584c80433369e6c373212c06
luke0b1000 <35980314+luke0b1000@users.noreply.github.com>
2018-11-06 20:10:25 -0500

    moved

--- problems21-30/26_ea0_leftRotation/index.js
+++ problems21-30/26_ea0_leftRotation/index.js
@@ -1,6 +1,6 @@
 /*
 https://www.hackerrank.com/challenges/array-left-rotation/problem
-
+Success! Just solved Left Rotation on @HackerRank. Can you complete the challenge? http://hr.gs/ea0 #programming
 */
 
 function leftRotation(myArray, rotateTimes) {
