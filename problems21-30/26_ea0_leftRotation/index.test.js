6c2ede6afd2a51bbb8d7c10e4f13b4d935472509
luke0b1000 <35980314+luke0b1000@users.noreply.github.com>
2018-11-06 20:10:43 -0500

    skip

--- problems21-30/26_ea0_leftRotation/index.test.js
+++ problems21-30/26_ea0_leftRotation/index.test.js
@@ -1,6 +1,6 @@
 const leftRotation = require('./index');
 
-describe('luke describe 1', () => {
+describe.skip('luke describe 1', () => {
     test('luke test 1', () => {
         expect(leftRotation([1, 2, 3, 4, 5], 4)).toEqual([5, 1, 2, 3, 4]);
     });
