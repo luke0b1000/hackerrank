80676a12b225a366b482e12d73ee38a80833dc44
luke0b1000 <35980314+luke0b1000@users.noreply.github.com>
2018-11-15 21:10:34 -0500

    saving

--- src/hold/_nyChaos/index.test.js
+++ src/hold/_nyChaos/index.test.js
@@ -1,15 +1,15 @@
-const balancedBrackets = require("./index");
-describe("luke describe 1", () => {
-    test("luke test 1", () => {
-        expect(balancedBrackets("{[()]}")).toEqual("YES");
+const balancedBrackets = require('./index');
+describe.skip('luke describe 1', () => {
+    test('luke test 1', () => {
+        expect(balancedBrackets('{[()]}')).toEqual('YES');
     });
-    test("luke test 2", () => {
-        expect(balancedBrackets("{[(])}")).toEqual("NO");
+    test('luke test 2', () => {
+        expect(balancedBrackets('{[(])}')).toEqual('NO');
     });
-    test("luke test 3k", () => {
-        expect(balancedBrackets("{{[[(())]]}}")).toEqual("YES");
+    test('luke test 3k', () => {
+        expect(balancedBrackets('{{[[(())]]}}')).toEqual('YES');
     });
-    test("luke test 5k", () => {
-        expect(balancedBrackets("}{")).toEqual("YES");
+    test('luke test 5k', () => {
+        expect(balancedBrackets('}{')).toEqual('YES');
     });
 });
